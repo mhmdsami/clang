66be5f7af2c001a099583d62ea8a8d8b126e3bf0
Mohamed Sami <mohamedsamis12a@gmail.com>
2022-03-03 23:20:52 +0530

    Add exercise 1

---
+++ ex1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* THis is a comment. */
+int main(int argc, char *argv[])
+{
+		int distance = 100;
+
+		// this is a comment
+		printf("You are %d miles away.\n", distance);
+
+		return 0;
+}
